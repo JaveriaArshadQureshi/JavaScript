ae6e3fedb39df9013e3b07dc75407fa1eeb1183b
Javeriabintearshad@gmail.com <javeriabintearshad@gmail.com>
2026-02-13 00:15:46 +0500

    examples

--- M2Section1.js
+++ M2Section1.js
@@ -9,3 +9,24 @@
 // the previous step. Finally, declare a variable in which you store the price of all your flowers (again, 
 // use the previous variables for initialization). Display all inventory information in the console in the 
 // following form:
+
+let rose_price = 8;
+let lily_price = 10;
+let tulip_price = 2;
+
+let totalno_roses = 70;
+let totalno_lilies = 50;
+let totalno_tuplis = 120;
+
+let value = rose_price * totalno_roses;
+console.log("Roses_total_price: ",value);
+
+let value1 = lily_price * totalno_lilies;
+console.log("Lilies_total_price: ",value);
+
+let value2 = tulip_price * totalno_tuplis;
+console.log("Tuplis_total_price: ",value);
+
+
+let total = value + value1 + value2
+console.log("Total: ", total)
