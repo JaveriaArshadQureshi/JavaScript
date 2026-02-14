670388dec1bd5e416daaf7e65b87aa0171eb82c1
Javeriabintearshad@gmail.com <javeriabintearshad@gmail.com>
2026-02-14 15:45:21 +0500

    examples

--- M2Section1.js
+++ M2Section1.js
@@ -10,13 +10,44 @@
 // use the previous variables for initialization). Display all inventory information in the console in the 
 // following form:
 
-let rosePrice = 8;
-let lilyPrice = 10;
-let tulipPrice = 2;
+// let rosePrice = 8;
+// let lilyPrice = 10;
+// let tulipPrice = 2;
+
+// let totalnoRoses = 70;
+// let totalnoLilies = 50;
+// let totalnoTuplis = 120;
+
+// let rosesValue = rosePrice * totalnoRoses;
+// console.log("Rose  unit price:", rosePrice, ", quantity:", totalnoRoses, ", value:", rosesValue);
+
+// let liliesValue = lilyPrice * totalnoLilies;
+// console.log("Lily  unit price:", lilyPrice, ", quantity:", totalnoLilies, ", value:", liliesValue);
+
+// let tulipsValue = tulipPrice * totalnoTuplis;
+// console.log("Tulip  unit price:", tulipPrice, ", quantity:", totalnoTuplis, ", value:", tulipsValue);
+
+
+// let total = rosesValue + liliesValue + tulipsValue
+// console.log("Total: ", total);
+
+
+// Question 2: Modify the code from the previous example.
+
+// Assume that the prices of flowers will be constant (they will not change). Declare and initialize the remaining variables in the same way as in the previous example. Display all the gathered information in the console. Now decrease the number of roses by 20 and lilies by 30.
+
+// Display all the collected information in the console again.
+
+const rosePrice = 8;
+const lilyPrice = 10;
+const tulipPrice = 2;
 
 let totalnoRoses = 70;
 let totalnoLilies = 50;
 let totalnoTuplis = 120;
+
+totalnoRoses = totalnoRoses - 20;
+totalnoLilies = totalnoLilies - 30;
 
 let rosesValue = rosePrice * totalnoRoses;
 console.log("Rose  unit price:", rosePrice, ", quantity:", totalnoRoses, ", value:", rosesValue);
