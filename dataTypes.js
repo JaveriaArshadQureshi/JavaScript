bb61dceefe4c45922f09ff900ddb502936e58f6a
Javeriabintearshad@gmail.com <javeriabintearshad@gmail.com>
2026-02-16 02:35:54 +0500

    new

--- dataTypes.js
+++ dataTypes.js
@@ -1,6 +1,18 @@
 console.log("Hello, World!")
 
-let year = 1990;
-console.log(year); // -> 1990
-console.log(1991); // -> 1991
-console.log("Alice"); // -> Alice
+// let year = 1990;
+// console.log(year); // -> 1990
+// console.log(1991); // -> 1991
+// console.log("Alice"); // -> Alice
+
+let  year  =  1990;
+// console.log(typeof  year);  //  ->  number
+// console.log(typeof  1991);  //  ->  number
+   
+// let  name  =  "Alice";
+// console.log(typeof  name);  //  ->  string
+// console.log(typeof  "Bob");  //  ->  string
+   
+let  typeOfYear  =  typeof  year;
+// console.log(typeOfYear);  //  ->  number
+console.log(typeof  typeOfYear);  //  ->  string
