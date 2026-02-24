0c613ddd8f8fb8aa479df0fe8cbc351c966f5e21
Javeriabintearshad@gmail.com <javeriabintearshad@gmail.com>
2026-02-24 23:46:46 +0500

    type

--- dataTypes.js
+++ dataTypes.js
@@ -36,17 +36,41 @@ let halfArea = area / 2;
 console.log(years); // -> 1991;
 console.log(typeof year); // -> number;
 
-let a = 10; // decimal - default
-let b = 0x10; // hexadecimal
-let c = 0o10; // octal
-let d = 0b10; // binary
+// let a = 10; // decimal - default
+// let b = 0x10; // hexadecimal
+// let c = 0o10; // octal
+// let d = 0b10; // binary
    
-console.log(a); // -> 10
-console.log(b); // -> 16
-console.log(c); // -> 8
-console.log(d); // -> 2
+// console.log(a); // -> 10
+// console.log(b); // -> 16
+// console.log(c); // -> 8
+// console.log(d); // -> 2
    
-let x = 9e3;
-let y = 123e-5;
-console.log(x); // -> 9000
-console.log(y); // -> 0.00123
+// let x = 9e3;
+// let y = 123e-5;
+// console.log(x); // -> 9000
+// console.log(y); // -> 0.00123
+
+
+let  a  =  1  /  0;
+let  b  =  -Infinity;
+   
+console.log(a);  //  ->  Infinity
+console.log(b);  //  ->  -Infinity
+console.log(typeof  a);  //  ->  number
+console.log(typeof  b);  //  ->  number
+   
+let  s  =  "it's  definitely  not  a  number";
+let  n  =  s  *  10;
+console.log(n);  //  ->  NaN
+console.log(typeof  n);  //  ->  number
+
+
+let  big  =  1234567890000000000000n;
+let  big2  =  1n;
+   
+console.log(big);  //  ->  1234567890000000000000n
+console.log(typeof  big);  //  ->  bigint
+   
+console.log(big2);  //  ->  1n
+console.log(7n  /  4n);  //  ->  1n
